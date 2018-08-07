26283cc3412e644e7731b0b85cb1a07ec26563a1
zhaoguojun <985531883@qq.com>
2018-08-07 17:43:23 +0800

    添加和less

---
+++ src/components/header/header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: 'header',
+  data(){
+      return {
+          title: 'header'
+      }
+  },
+  created(){
+      
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
+
